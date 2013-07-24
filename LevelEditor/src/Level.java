0302da4dcb7acb29cb42184ec9710a6d10d851d6
Adam Magleby <amaglebymd@gmail.com>
2013-07-23 18:17:53 -0700

    updates to the Level Editor
    -reworked structure, commented, removed unnecessary files

--- LevelEditor/src/Level.java
+++ LevelEditor/src/Level.java
@@ -1,5 +1,13 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPopupMenu;
 
 /*
  * this will hold all the waves, so full of enemies... to kill!
@@ -7,20 +15,51 @@ import java.util.List;
 
 public class Level {
 
-	int levelNumber;
-	public List<Wave> waveList = new ArrayList<Wave>();
+	int levelNumber; //number of the level
+	String levelName;  //name of the level
+	public List<Wave> waveList = new ArrayList<Wave>(); //list of waves contained in this level
 	
-	//Constructor
-	public Level (int level )
+	JMenuItem newWaveButton = new JMenuItem("New Wave"); //button to create a new wave
+	public JMenu levelWavesMenu = null; 
+	JPopupMenu wavePopup = null; //popup menu to add name
+	JFrame waveFrame = new JFrame(); //JFrame to hold the popup menu
+	
+	//Constructor for a new Level
+	public Level (String name, int level )
 	{
 		levelNumber = level;
+		levelName = name;
+		levelWavesMenu = new JMenu(name);
+		
+		//menu set up
+		newWaveButton.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {  //this part is only run once it is clicked
+				String s = (String)JOptionPane.showInputDialog( //set up for the popup menu
+	                    waveFrame,
+	                    "Enter a time for when this wave should appear.",
+	                    "Time Selection",
+	                    JOptionPane.PLAIN_MESSAGE, null,
+	                    null, "");
+				int t = Integer.parseInt(s);
+				Wave newWave = new Wave(t);	 //create a new wave
+				waveList.add(newWave); //take that wave and add to this levels waveList
+				levelWavesMenu.add(newWave.waveButton); //add the newWave's waveButton to the level's wave menu
+			}
+		});
+		
+		levelWavesMenu.add(newWaveButton); //add the newWaveButton to the levelWavesMenu
 	}
 	
+	//adds a single wave to the Levels waveList field
+	/*    PENDING REMOVAL
 	public void AddWave(Wave newWave)
 	{
 		System.out.println("Adding Wave");
 		waveList.add(newWave);
 		System.out.println("Size of Level: " + waveList.size());
 	}
+	*/
+	
 	
 }
--- LevelEditor/src/Wave.java
+++ LevelEditor/src/Wave.java
@@ -1,23 +1,39 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.swing.JMenuItem;
+//import javax.swing.JOptionPane;
 
 
 public class Wave {
 
-	public int time = 0;
+	public int time = 0; //the time that the wave will begin
 	private List<Enemy> waveEnemyList = new ArrayList<Enemy>();
+	JMenuItem waveButton = null;
 	
-	public Wave(List<Enemy> list, int t){
+	//constructor for wave
+	public Wave( int t){
 		time = t;
-		waveEnemyList = list;
+		
+		//menu set up
+		waveButton = new JMenuItem(Integer.toString(t));
+		waveButton.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//will make a call to load wave
+				
+			}
+		});
 	}
 	
-	/*
-	public void loadWave(){
-		System.out.println("LOADED BITCHES");
-		WaveScreen.SetEnemyList(waveEnemyList);
+	public void loadWave()
+	{
+		//need to set the currentWave to this newly created wave
+			
 	}
-	*/
 	
 	public List<Enemy> getWave(){
 		return waveEnemyList;
