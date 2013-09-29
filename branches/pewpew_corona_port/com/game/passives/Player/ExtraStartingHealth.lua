9e5d37494b12e511eb7614439931e07a0e81ce31
Ian Nei <iancnei@gmail.com>
2013-09-28 18:30:14 -0700

    Made the Health Regen Passive have a cooldown between healing.  Added in asserts into ExtraStartingHealth.lua and HealthRegen.lua.  Made it so that default values are used for the player's passives in Player.lua.

--- branches/pewpew_corona_port/com/game/passives/Player/ExtraStartingHealth.lua
+++ branches/pewpew_corona_port/com/game/passives/Player/ExtraStartingHealth.lua
@@ -1,14 +1,16 @@
 require "com.game.passives.Passive"
 ExtraStartingHealth = Passive:subclass("ExtraStartingHealth")
 
-local EXTRA_DEFUALT_VALUE = 1
+local EXTRA_DEFAULT_VALUE = 1
 
 function ExtraStartingHealth: init(objectRef, increaseAmount)
+	assert(objectRef, "In Extra Starting Health passive, object reference is incorrect.")
+	assert(objectRef["health"], "In Extra Starting Health passive, object reference does not have a health field.")
 	if objectRef ~= nil then
 		self.super:init(objectRef, "health")
 		
 		if increaseAmount == nil then
-			increaseAmount = EXTRA_DEFUALT_VALUE
+			increaseAmount = EXTRA_DEFAULT_VALUE
 		end
 		
 		self.objectRef[self.fieldName] = self.objectRef[self.fieldName] + increaseAmount 
