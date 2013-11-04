824636c14ad7b8e03daa4c32f8abd5b9a4778ac8
Nick Patti <nickspatties@gmail.com>
2013-11-03 17:37:52 -0800

    Added images to the codec! It took a really long time to finish this. D:

--- branches/codectesting/codec.lua
+++ branches/codectesting/codec.lua
@@ -93,7 +93,7 @@ function caFadeOut()
 		ca,
 		{
 			time=500,
-			alpha=0,
+			alpha=0.1,
 			transition=easing.inOutQuad,
 			onComplete=caFadeIn
 		}
@@ -186,6 +186,27 @@ c_p:setStrokeColor(200, 200, 200)
 c_dg:insert(c_p)
 
 
+-- c_p_img: codec portrait image
+local c_p_imgtable = {
+	Reggie 	= display.newImageRect(M.c_p_img["ReggiePath"], M.c_p_w, M.c_p_w),
+	Tyce	= display.newImageRect(M.c_p_img["TycePath"], M.c_p_w, M.c_p_w)
+}
+
+-- c_p_reggie: reggie's portrait image
+local c_p_reggie	= c_p_imgtable["Reggie"]
+c_p_reggie.x 		= M.c_p_img_x
+c_p_reggie.y 		= M.c_p_img_y
+c_p_reggie.alpha	= 0
+
+-- c_p_tyce: tyce's portrait image
+local c_p_tyce		= c_p_imgtable["Tyce"]
+c_p_tyce.x 			= M.c_p_img_x
+c_p_tyce.y 			= M.c_p_img_y
+c_p_tyce.alpha		= 0
+c_dg:insert(c_p_reggie)
+c_dg:insert(c_p_tyce)
+
+
 -- c_p_txt: codec portrait text
 local c_p_txt = display.newText(
 	MSGS[msg_c].name,				-- text
@@ -220,12 +241,16 @@ end
 
 -- codec assets populating listener
 function makeCodecAssetsAppear(obj)
+
+	-- make text appear
 	c_txt.alpha   = 1
 	c_p_txt.alpha = 1
 	
+	-- make portrait appear
 	local speaker = MSGS[msg_c].name
 	local c_p_rgb = M.c_p_img[speaker]
 	c_p:setFillColor(c_p_rgb.r, c_p_rgb.g, c_p_rgb.b)
+	c_p_imgtable[speaker].alpha = 1
 end
 
 
@@ -249,16 +274,23 @@ function c_dg:touch(event)
 		
 			-- play the advance sound effect
 			audio.play(sfx.advance)
+			
+			-- make the previous image go away
+			local previousSpeaker = MSGS[msg_c].name
+			c_p_imgtable[previousSpeaker].alpha = 0
 		
-			-- change the message and/or portrait in codec
+			-- grab the information for the next message
 			msg_c = msg_c + 1
 			local speaker = MSGS[msg_c].name
 			local message = MSGS[msg_c].content
 			local c_p_rgb = M.c_p_img[speaker]
 			
+			-- change the message and portrait in the codec
 			c_txt.text    = message
 			c_p_txt.text  = speaker
 			c_p:setFillColor(c_p_rgb.r, c_p_rgb.g, c_p_rgb.b)
+			c_p_imgtable[speaker].alpha = 1
+			
 			
 		else
 		
