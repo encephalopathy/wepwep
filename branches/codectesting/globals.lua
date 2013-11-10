65ee6033ab7c7524d2cd115a392bea993954d1b4
Nick Patti <nickspatties@gmail.com>
2013-11-10 13:43:29 -0800

    Changed the position and look of the speaker's name in the codec. Starting to implement different expressions in the codec. Will get to file reading eventually...

--- branches/codectesting/globals.lua
+++ branches/codectesting/globals.lua
@@ -20,19 +20,26 @@ M.c_txtb_h  = 125
 M.c_txtb_sw = 3			-- sw: stroke width
 
 -- c_txt: codec text
--- NOTE: codec messages are now assigned in messagesreader.lua
 M.c_txt_x      = 210
-M.c_txt_y      = 635
+M.c_txt_y      = 660
 M.c_txt_w      = 240
 M.c_txt_h      = h - 175
 M.c_txt_fs     = 20		-- fs: font size
 
 -- c_p: codec portrait
-M.c_p_img = {			-- c_p_img: portrait image (this will be replaced with strings for images)
-	Reggie = {r = 225, g = 100, b = 100},
-	Tyce   = {r = 100, g = 225, b = 100},
-	ReggiePath 	= "images/face01.png",
-	TycePath	= "images/face02.png"
+M.c_p_img = {			-- c_p_img: portrait image, includes colors and pathnames
+
+	-- each of these tables contains pathnames for portrait images and color values
+	-- for each character and their expressions
+	Reggie = {
+		color		= {r = 225, g = 100, b = 100},
+		neutral		= "images/face01.png"
+	},
+	
+	Tyce   = {
+		color 		= {r = 100, g = 225, b = 100},
+		neutral		= "images/face02.png"
+	}
 }
 M.c_p_x  = 75
 M.c_p_y  = h - 175 + 200
@@ -44,7 +51,7 @@ M.c_p_img_x = 75 + 125 * 0.5
 M.c_p_img_y = (h - 175 + 200) + 125 * 0.5
 
 -- c_p_t: codec portrait text
-M.c_p_t_x = 85
-M.c_p_t_y = 720
+M.c_p_t_x = 210
+M.c_p_t_y = 635
 
 return M
