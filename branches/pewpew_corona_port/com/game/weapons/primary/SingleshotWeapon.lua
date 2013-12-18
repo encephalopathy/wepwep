7f0ec20c965149d606eac5cd1cc94f4e0fb7ca66
Ian Nei <iancnei@gmail.com>
2013-12-17 16:45:32 -0800

    Changed a few default values in regards to how weapons shoot in waves.  There should be less delay now when one first starts firing.

--- branches/pewpew_corona_port/com/game/weapons/primary/SingleshotWeapon.lua
+++ branches/pewpew_corona_port/com/game/weapons/primary/SingleshotWeapon.lua
@@ -57,19 +57,19 @@ function Singleshot:init (sceneGroup, isPlayerOwned, rateOfFire, bulletSpeed, nu
   
    --these values are for haters to give them delay
    if numberOfWaves == nil then
-      self.numberOfWaves = 3
+      self.numberOfWaves = 0
    else
       self.numberOfWaves = numberOfWaves
    end
    
    if delayBetweenWaves == nil then
-      self.delayBetweenWaves = 3
+      self.delayBetweenWaves = 0
    else
       self.delayBetweenWaves = delayBetweenWaves
    end
    
-	self.waveCounter = 3
-	self.delayCounter = 3
+	self.waveCounter = 0
+	self.delayCounter = 0
    
    --load it here with a string to sound file
 end
@@ -114,7 +114,7 @@ function Singleshot:fire()
 		if self.isPlayerOwned == true then
 			--print("PLAYER OWNED. FIRE SOUNDS")
 			--self:playFiringSound(self.soundFX) --call to play sound for weapons
-			if self.waveCounter <= self.numberOfWaves and self.delayCounter == 0 then
+			-if self.waveCounter <= self.numberOfWaves and self.delayCounter == 0 then
 				local rotationAngle = math.rad(self.owner.sprite.rotation)
 				self:calibrateMuzzleFlare(self.muzzleLocation.x, self.muzzleLocation.y, self.owner, bullet, rotationAngle)
 				local bulletVelocity = self:calculateBulletVelocity(bullet, self.owner)
