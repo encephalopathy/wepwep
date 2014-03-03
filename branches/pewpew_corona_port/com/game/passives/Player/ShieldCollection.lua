846a84996ee26ed83589ed8d4b6f93d76e24e6b5
Ian Nei <iancnei@gmail.com>
2014-03-02 17:51:47 -0800

    Renamed ShieldCollection since it is serving double duty now.  Added in code for the isActivatable field into GunpodCollection.lua and updated Shop.lua to reflect that change.

--- branches/pewpew_corona_port/com/game/passives/Player/ShieldCollection.lua
+++ branches/pewpew_corona_port/com/game/passives/Player/ShieldCollection.lua
@@ -1,33 +1,35 @@
 require "com.game.passives.Player.PassiveShield"
+require "com.game.passives.Player.ActivatableShield"
 
- PassiveShieldCollection= Passive:subclass("PassiveShieldCollection")
+ShieldCollection = Passive:subclass("ShieldCollection")
 
- function PassiveShieldCollection:init(spriteImage, shieldType)
+function ShieldCollection:init(isActivatable, spriteImage, shieldType)
+	self.super:init(nil, isActivatable)
  	self.collection = {}
  	self.spriteImage = spriteImage
  	self.shieldType = shieldType
- end
+end
 
- function PassiveShieldCollection:setOwner(objectRef, sceneGroup)
- 	assert(objectRef ~= nil, 'PassiveShieldCollection: Did not equip an owner such as player or an enemy to this passive')
- 	assert(sceneGroup ~=nil, "PassiveShieldCollection: sceneGroup is nil")
+function ShieldCollection:setOwner(objectRef, sceneGroup)
+ 	assert(objectRef ~= nil, 'ShieldCollection: Did not equip an owner such as player or an enemy to this passive')
+ 	assert(sceneGroup ~=nil, "ShieldCollection: sceneGroup is nil")
  	self.objectRef = objectRef
  	self.oldPositionX = self.objectRef.sprite.x
 	self.oldPositionY = self.objectRef.sprite.y
 	print("self.sizeX & Y is", self.sizeX, self.sizeY)
 	self.collection[1] = self.shieldType:new(sceneGroup, self.spriteImage, self.objectRef, self.objectRef.sprite.width + 100, self.objectRef.sprite.height + 100)
- end
+end
 
- function PassiveShieldCollection:update()
+function ShieldCollection:update()
  	self.collection[1]:update(self.oldPositionX, self.oldPositionY)
  	self.oldPositionX = self.objectRef.sprite.x
  	self.oldPositionY = self.objectRef.sprite.y
- end
+end
 
- function PassiveShieldCollection:clear()
+function ShieldCollection:clear()
 	for i = 1, #self.collection, 1 do
 		self.collection[i]:destroy()
 	end
- end
+end
 
- return PassiveShieldCollection
+return ShieldCollection
