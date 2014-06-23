d77c457867b1edec885a352f95f52cc79bbe0aa5
BenMadness <mysoldiermadness@gmail.com>
2014-06-22 20:30:22 -0700

    adding in sounds

--- branches/pewpew_corona_port/com/game/gameSFXInfo.lua
+++ branches/pewpew_corona_port/com/game/gameSFXInfo.lua
@@ -26,21 +26,21 @@
 --setting = Solo: this sound will turn off all non-BGM channels and play only this sound
 --setting = NRML: this sound does not have a specified behavior and will seek out a free channel to play on
  gameSFXInfo = {
-	Singleshot = {path = "com/resources/music/soundfx/laser.ogg", channel = 2, setting = 'R'},
-	Doubleshot = {path = "com/resources/music/soundfx/doubleShot.ogg", channel = 2, setting = 'R'},
+	Singleshot = {path = "com/resources/music/soundfx/BenSounds/Laser1.wav", channel = 2, setting = 'R'},
+	Doubleshot = {path = "com/resources/music/soundfx/BenSounds/DoubleLaser1.wav", channel = 2, setting = 'R'},
 	SineWave = {path = "com/resources/music/soundfx/sineWave.ogg", channel = 2, setting = 'R'},
-	Spreadshot = {path = "com/resources/music/soundfx/sineWave.ogg", channel = 2, setting = 'R'},
-	Homingshot = {path = "com/resources/music/soundfx/laser.ogg", channel = 2, setting = 'R'},
-	Randomshot = {path = "com/resources/music/soundfx/shotgun.ogg", channel = 2, setting = 'R'},
-	SpiralStraightshot = {path = "com/resources/music/soundfx/laser.ogg", channel = 2, setting = 'R'},
-	SpiralCurveshot = {path = "com/resources/music/soundfx/shotgun.ogg", channel = 2, setting = 'R'},
-	Circleshot = {path = "com/resources/music/soundfx/shotgun.ogg", channel = 2, setting = 'R'},
-	Backshot = {path = "com/resources/music/soundfx/shotgun.ogg", channel = 2, setting = 'R'},
-	Bomb = {path = "com/resources/music/soundfx/shotgun.ogg", channel = nil, setting = 'NRML'},
-	Hater_die = {path = "com/resources/music/soundfx/enemyDeath.ogg", channel = 3, setting = 'R'},
-	Hater_onHit = {path = "com/resources/music/soundfx/enemyHit.ogg", channel = 4, setting = 'NR'},
-	HealthPickUp = {path = "com/resources/music/soundfx/healthTemp.ogg", channel = 5, setting = 'R'},
-	EnergyPickUp = {path = "com/resources/music/soundfx/energyTemp.ogg", channel = 5, setting = 'R'},
-	ScrapPickUp = {path = "com/resources/music/soundfx/ring.ogg", channel = 5, setting = 'R'},
-	Player_onHit = {path = "com/resources/music/soundfx/playerHitTemp.ogg", channel = 6, setting = 'R'}
+	Spreadshot = {path = "com/resources/music/soundfx/BenSounds/Spreadshot.ogg", channel = 2, setting = 'R'},
+	Homingshot = {path = "com/resources/music/soundfx/BenSounds/laser.ogg", channel = 2, setting = 'R'},
+	Randomshot = {path = "com/resources/music/soundfx/BenSounds/shotgun2weak.ogg", channel = 2, setting = 'R'},
+	SpiralStraightshot = {path = "com/resources/music/soundfx/BenSounds/Laser1.wav", channel = 2, setting = 'R'},
+	SpiralCurveshot = {path = "com/resources/music/soundfx/BenSounds/Laser1.wav", channel = 2, setting = 'R'},
+	Circleshot = {path = "com/resources/music/soundfx/BenSounds/Laser3.wav", channel = 2, setting = 'R'},
+	Backshot = {path = "com/resources/music/soundfx/BenSounds/shotgun2weak.wav", channel = 2, setting = 'R'},
+	Bomb = {path = "com/resources/music/soundfx/BenSounds/Bomb.wav", channel = nil, setting = 'NRML'},
+	Hater_die = {path = "com/resources/music/soundfx/BenSounds/HaterDeath.wav", channel = 3, setting = 'R'},
+	Hater_onHit = {path = "com/resources/music/soundfx/BenSounds/HaterHit.wav", channel = 4, setting = 'NR'},
+	HealthPickUp = {path = "com/resources/music/soundfx/BenSounds/healthTemp.wav", channel = 5, setting = 'R'},
+	EnergyPickUp = {path = "com/resources/music/soundfx/BenSounds/energyPickup.wav", channel = 5, setting = 'R'},
+	ScrapPickUp = {path = "com/resources/music/soundfx/BenSounds/CoinPickupBitCrushed.wav", channel = 5, setting = 'R'},
+	Player_onHit = {path = "com/resources/music/soundfx/BenSounds/PlayerHit.wav", channel = 6, setting = 'R'}
 }
